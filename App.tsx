b45861dca194f32b40197d4748f51be09ff08d87
Mohamed Ali <im_bousba@esi.dz>
2022-10-22 12:19:31 +0100

    add: splash screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
-import {SafeAreaView, Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigationStack from './src/components/navigations/MainNavigationStack';
 import Login from './src/screens/LoginScreen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+  
   return (
     <NavigationContainer>
       <MainNavigationStack />
--- android/app/src/main/java/com/ldrnassignment/MainActivity.java
+++ android/app/src/main/java/com/ldrnassignment/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ldrnassignment;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -24,6 +27,12 @@ public class MainActivity extends ReactActivity {
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
